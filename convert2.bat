bc6e1ed12b5aa2af980969de0efa0a7b4e77390b
Tobey Luciando <42970574+symeRobinson@users.noreply.github.com>
2020-04-09 08:59:19 -0500

    Update convert2.bat

--- convert2.bat
+++ convert2.bat
@@ -3,26 +3,37 @@ setlocal enabledelayedexpansion
 set mypath=%cd%
 set count=0
 set othercount=0
+
 for /r %%x in (*.wdr) do set /a count+=1
 for /r %%x in (*.wbn) do set /a count+=1
 for /r %%x in (*.wtd) do set /a count+=1
+for /r %%x in (*.wbd) do set /a count+=1
+for /r %%x in (*.wdd) do set /a count+=1
 
+for /r %%v in (*.wbd) do set /a othercount+=1 & (
+	C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\fivem.com formats:convert %%v --game=ny
+	rem ping 127.255.255.255 -n 1 -w 200> nul
+	echo Converted %%v
+	echo !othercount! out of %count%
+)
 for /r %%v in (*.wdr) do set /a othercount+=1 & (
 	C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\fivem.com formats:convert %%v --game=ny
 	rem ping 127.255.255.255 -n 1 -w 200> nul
 	echo Converted %%v
 	echo !othercount! out of %count%
 )
-PING -n 5 127.0.0.1>nul
-echo "Finished with WDR files"
+for /r %%v in (*.wdd) do set /a othercount+=1 & (
+	C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\fivem.com formats:convert %%v --game=ny
+	rem ping 127.255.255.255 -n 1 -w 200> nul
+	echo Converted %%v
+	echo !othercount! out of %count%
+)
 for /r %%v in (*.wbn) do set /a othercount+=1 & (
 	C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\fivem.com formats:convert %%v --game=ny
 	rem ping 127.255.255.255 -n 1 -w 200> nul
 	echo Converted %%v
 	echo !othercount! out of %count%
 )
-PING -n 5 127.0.0.1>nul
-echo "Finished with WBN files"
 for /r %%v in (*.wtd) do set /a othercount+=1 & (
 	C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.app\fivem.com formats:convert %%v --game=ny
 	rem ping 127.255.255.255 -n 1 -w 200> nul
@@ -44,4 +55,3 @@ del "%mypath%\files\*.wtf" /s /f /q
 del "%mypath%\files\*.wnv" /s /f /q
 echo "Finished!"
 pause
-
